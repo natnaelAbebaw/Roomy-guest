b1763fd4b19a81fba5aed063ad8d6e6434ebe950
natnaelAbebaw <natnaell70a@gmail.com>
2024-11-12 00:46:17 +0300

    add about compnat

---
+++ src/features/cabins/About.tsx
@@ -0,0 +1,48 @@
+import Flex, { FlexAlign, FlexDirection } from "../../ui/Flex";
+import { Color, Font, Spacing } from "../../ui/cssConstants";
+import Heading, { HeadingElement } from "../../ui/Heading";
+import EllipsisParagraph from "../../ui/EllipsisParagraph";
+import Modal from "../../ui/Modal";
+import Button, { ButtonType } from "../../ui/Button";
+import { FaAngleRight } from "react-icons/fa";
+import Container from "../../ui/Container";
+
+function About({
+  title,
+  description,
+}: {
+  title: string;
+  description?: string;
+}) {
+  return (
+    <Flex mb={Spacing.s24} gap={Spacing.s8} direction={FlexDirection.Column}>
+      <Heading fs={Font.fs16} mb={Spacing.zero} as={HeadingElement.H2}>
+        About the {title}
+      </Heading>
+      <EllipsisParagraph>{description}</EllipsisParagraph>
+
+      <Modal>
+        <Modal.Open open="about">
+          <Button buttonType={ButtonType.Normal} color={Color.grey800}>
+            <Flex align={FlexAlign.Center}>
+              <span>Read More </span>
+              <FaAngleRight />
+            </Flex>
+          </Button>
+        </Modal.Open>
+        <Modal.Window name="about">
+          <Container padding={[Spacing.s8, Spacing.s32]}>
+            <Flex gap={Spacing.s12} direction={FlexDirection.Column}>
+              <Heading fs={Font.fs18} mb={Spacing.s1}>
+                About this {title}
+              </Heading>
+              <p>{description}</p>
+            </Flex>
+          </Container>
+        </Modal.Window>
+      </Modal>
+    </Flex>
+  );
+}
+
+export default About;
